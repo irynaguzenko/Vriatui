141ffa98a13be76faabd1bb7cdf6db232c2b220c
irynaziabkina <irynaguzenko1@gmail.com>
2018-07-21 13:39:43 +0300

    init project structure

---
+++ src/main/java/com/testname/vriatui/VriatuiApplication.java
@@ -0,0 +1,12 @@
+package com.testname.vriatui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VriatuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VriatuiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/testname/vriatui/VriatuiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.testname.vriatui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VriatuiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
